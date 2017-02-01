6107b065123cdbcf063f954c33bb3a83580fb45c
sagae keiga <sagae5.28rujeae@gmail.com>
2017-02-01 06:49:48 +0000

    first commit

---
+++ config/unicorn.rb
@@ -0,0 +1,30 @@
+
+
+listen File.expand_path('../../tmp/sockets/unicorn.sock', __FILE__)
+
+pid "tmp/pids/unicorn.pid"
+
+worker_processes 2
+
+timeout 15
+
+preload_app true
+
+ROOT = File.dirname(File.dirname(__FILE__))
+stdout_path "#{ROOT}/log/unicorn-stdout.log"
+stderr_path "#{ROOT}/log/unicorn-stderr.log"
+
+before_fork do |server, worker|
+    defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
+    old_pid = "#{ server.config[:pid] }.oldbin"
+    unless old_pid == server.pid
+      begin
+        Proccess.kill :QUIT, File.read(old_pid).to_i
+      rescue Errno::ENOENT, Errno::ESERCH
+      end
+    end
+end
+
+after_fork do |server, worker|
+    defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
+end
