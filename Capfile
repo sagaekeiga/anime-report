e84b88bbe96b641dd8941d43cd0a41d54276a221
sagae keiga <sagae5.28rujeae@gmail.com>
2017-02-02 01:45:03 +0000

    listen

--- Capfile
+++ Capfile
@@ -7,6 +7,7 @@ require 'capistrano/rails/assets'
 require 'capistrano/rails/migrations'
 require 'capistrano/rbenv'
 require 'capistrano/bundler'
+require 'capistrano-unicorn'
 
 
 install_plugin Capistrano::SCM::Git
--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,24 +1,23 @@
-
-
-rails_root = File.expand_path('../../', __FILE__)
-ENV['BUNDLE_GEMFILE'] = rails_root + "/Gemfile"
-listen File.expand_path('../../tmp/sockets/unicorn.sock', __FILE__)
-
-pid "tmp/pids/unicorn.pid"
-
+listen "160.16.198.30:61203"
+pid "tmp/pids/unicron.pid"
 worker_processes 2
-
 timeout 15
-
 preload_app true
 
 ROOT = File.dirname(File.dirname(__FILE__))
 stdout_path "#{ROOT}/log/unicorn-stdout.log"
 stderr_path "#{ROOT}/log/unicorn-stderr.log"
 
-root = "/var/www/myapp/current"
-before_exec do |server|
-    ENV['BUNDLE_GEMFILE'] = "#{root}/Gemfile"
+before_fork do |server, worker|
+    defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
+    
+    old_pid = "#{ server.config[:pid] }.oldbin"
+    unless old_pid == server.pid
+      begin
+        Process.kill :QUIT, File.read(old_pid).to_i
+      rescue Errno::ENOENT, Errno::ESRCH
+      end
+    end
 end
 
 after_fork do |server, worker|
