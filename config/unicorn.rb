aba09bc34e1ac490330b3e57918eb9ee3f44bdcf
sagae keiga <sagae5.28rujeae@gmail.com>
2017-02-02 02:38:48 +0000

    new

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,24 +1,27 @@
+APP_PATH = "/home/anime/app/anime-report"
 
-worker_processes 2
-timeout 15
+worker_processes 4
+working_directory APP_PATH
+listen "/var/run/unicorn/unicorn.socket"
+pid APP_PATH + "/tmp/pids/unicorn.pid"
+stderr_path APP_PATH + "/log/unicorn.log"
+stdout_path APP_PATH + "/log/unicorn.log"
+
 preload_app true
 
-ROOT = File.dirname(File.dirname(__FILE__))
-stdout_path "#{ROOT}/log/unicorn-stdout.log"
-stderr_path "#{ROOT}/log/unicorn-stderr.log"
-
 before_fork do |server, worker|
-    defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
-    
-    old_pid = "#{ server.config[:pid] }.oldbin"
-    unless old_pid == server.pid
-      begin
-        Process.kill :QUIT, File.read(old_pid).to_i
-      rescue Errno::ENOENT, Errno::ESRCH
-      end
+  ActiveRecord::Base.connection.disconnect!
+
+  old_pid = "#{server.config[:pid]}.oldbin"
+  if old_pid != server.pid
+    begin
+      sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
+      Process.kill(sig, File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
     end
+  end
 end
 
 after_fork do |server, worker|
-    defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
+  ActiveRecord::Base.establish_connection
 end
