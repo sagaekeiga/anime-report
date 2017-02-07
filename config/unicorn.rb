18d4a3e51011ef90e453d00aeccff54c5d624cfb
sagae keiga <sagae5.28rujeae@gmail.com>
2017-02-07 13:09:19 +0000

    unicorn-finish

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,27 +1,7 @@
-APP_PATH = "/home/anime/app/anime-report/current"
+worker_processes 2
+working_directory "/home/anime/app/anime-report"
 
-worker_processes 4
-working_directory APP_PATH
-listen "/var/run/unicorn/unicorn.socket"
-pid APP_PATH + "/tmp/pids/unicorn.pid"
-stderr_path APP_PATH + "/log/unicorn.log"
-stdout_path APP_PATH + "/log/unicorn.log"
+listen "/var/run/unicorn/unicorn.sock"
+pid "/var/run/unicorn/unicorn.pid"
 
 preload_app true
-
-before_fork do |server, worker|
-  ActiveRecord::Base.connection.disconnect!
-
-  old_pid = "#{server.config[:pid]}.oldbin"
-  if old_pid != server.pid
-    begin
-      sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
-      Process.kill(sig, File.read(old_pid).to_i)
-    rescue Errno::ENOENT, Errno::ESRCH
-    end
-  end
-end
-
-after_fork do |server, worker|
-  ActiveRecord::Base.establish_connection
-end
