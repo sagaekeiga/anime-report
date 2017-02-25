7e55f2562f0cc97aaa00b9c2591ec4905cc9b724
sagae keiga <sagae5.28rujeae@gmail.com>
2017-02-25 07:23:30 +0000

    redis

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -5,5 +5,8 @@ listen "/var/run/unicorn/unicorn.sock"
 pid "/var/run/unicorn/unicorn.pid"
 
 
+stderr_path "/home/anime/deploy/current/log/unicorn.log"
+stdout_path "/home/anime/deploy/current/log/unicorn.log"
+
 
 preload_app true
