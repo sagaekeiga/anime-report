1b561bb9c0eafe2fd03e34e210f4706b0a3a0d61
sagae keiga <sagae5.28rujeae@gmail.com>
2017-03-02 12:05:44 +0000

    redis

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,19 +3,7 @@ class PagesController < ApplicationController
 
   def index
     @q = Work.search(params[:q])
-    
     @works = Work.all
-    
-    # 個別記事のPV数を取り出す
-    @works.each do |work|
-        @daily_pageviews[work.id] = REDIS.get "works/daily/#{today}/#{work.id}"
-    end
-    
-    # PV数のソーティング
-    @daily_pageviews.sort_by{|k, v| v} if !@daily_pageviews.blank?
-    
-    #上位10個の記事を返す
-    @top10_pages = @daily_pageviews[0..10] if !@daily_pageviews.blank?
 
     ids = REDIS.zrevrangebyscore "works/daily/#{Date.yesterday.to_s}", "+inf", 0, limit: [0, 5]
     @ranking_articles = ids.map{ |id| Work.find(id) }
