1e6987b9975551ccadd86cd933754efcfd0b2917
sagae keiga <sagae5.28rujeae@gmail.com>
2017-03-04 11:13:29 +0000

    whenever

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,4 +1,4 @@
-env :GEM_PATH, ENV['GEM_PATH']
+env :PATH, ENV['PATH']
 require File.expand_path(File.dirname(__FILE__) + "/environment")
 set :output, 'log/crontab.log'
 set :environment, :production
